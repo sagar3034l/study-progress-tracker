5def467d5b216e59360aa30d8e11fd93947c9dfa
sagar3034l <sd8778610@gmail.com>
2026-08-22 17:14:26 +0530

    fix: correct typo in development mode check for API_URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,15 +1,11 @@
 import axios from 'axios'
 
-const API_URL = import.meta.env.MODE === "developement" ? "http://localhost:3000/api" : "/api"
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api"
 
 export const api = axios.create({
     baseURL: API_URL,
     withCredentials: true,
     headers: { "Content-type": "application/json"},
 })
-
-
-// import.meta.env.API_URL || "http://localhost:3000/api"
-
 
 
