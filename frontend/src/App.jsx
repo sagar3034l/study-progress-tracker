ba5bdfd7eb234a2fe9d374338d079d16704360ba
sagar3034l <sd8778610@gmail.com>
2026-08-27 19:08:04 +0530

    solve navigate issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,8 +16,7 @@ function App() {
   const location = useLocation()
   const protectedRoute = location.pathname === '/study' || location.pathname === '/studychart'
 
-
-  if (protectedRoute && !authReady) {
+  if (!authReady && (protectedRoute || location.pathname === '/')) {
     return <LoaderIcon className='animate-spin text-center relative top-[260px] left-[620px] size-24 text-amber-400' />
   }
 
